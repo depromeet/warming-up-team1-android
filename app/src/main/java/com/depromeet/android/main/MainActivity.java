f7389605dc32fd5ccf00315464775b22c25ec973
Mung <munkyung0324@gmail.com>
2019-10-24 13:12:27 +0900

    main layout 구성

--- app/src/main/java/com/depromeet/android/main/MainActivity.java
+++ app/src/main/java/com/depromeet/android/main/MainActivity.java
@@ -1,4 +1,4 @@
-package com.depromeet.android;
+package com.depromeet.android.main;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,6 +8,8 @@ import android.content.pm.Signature;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
+
+import com.depromeet.android.R;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
